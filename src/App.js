6edddf77cf6fdd2855f138229ea66b70128bc945
Dhruvesh <dhruveshchauhan2004@gmail.com>
2026-03-07 20:09:45 +0000

    edited comment

--- src/App.js
+++ src/App.js
@@ -325,8 +325,8 @@ function App() {
    
 
 
-    
-    // Add otehr code above this line as below it is the UI and we want to keep a clean structure
+
+    // Add other code above this line as below it is the UI and we want to keep a clean structure
 
 
     // ---------- HOME ----------
