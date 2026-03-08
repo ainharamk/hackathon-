55b749b3c4fb84e5e5d9bfa8f1993c4dfa079e9b
Jusxjc <198653346+jusxjc@users.noreply.github.com>
2026-03-08 22:40:57 +0000

    altered database variables to fit enviroment variables on railway

--- server/server.js
+++ server/server.js
@@ -23,10 +23,7 @@ const db = mysql.createPool({
   port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  database: process.env.DB_NAME
 });
 
 db.getConnection((err, connection) => {
