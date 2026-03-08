baf76d7767ce86cea06e0a6737df3abb9ae780c1
Jusxjc <198653346+jusxjc@users.noreply.github.com>
2026-03-08 18:23:41 +0000

    configured railway database connection

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 
@@ -6,12 +7,12 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
-// MySQL connection pool
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "Password1.",    // change this password to work on your servers
-    database: "hackathon",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
